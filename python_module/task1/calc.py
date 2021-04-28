5223b5610be5b3bae8ee6b6aceb92dbd4e0802d7
Yuliia Kopysova <julykopysova97@gmail.com>
2021-04-28 11:48:40 +0300

    Changed style docstrings

--- python_module/task1/calc.py
+++ python_module/task1/calc.py
@@ -1,109 +1,105 @@
-"""This module performs 4 basic math operation:
+"""
+This module performs 4 basic math operation:
 add, subtract, multiply, divide on two float numbers
-
 """
 
 
 class Calculator:
     """
-        A class used to represent a Calculator
+A class used to represent a Calculator
 
-        ...
+Attributes
+first_num : float
+    the first number for performing operations
+second_num : float
+    the second number for performing operations
 
-        Attributes
-        ----------
-        first_num : float
-            the first number for performing operations
-        second_num : float
-            the second number for performing operations
-
-        Methods
-        -------
-        __init__(self, first_num, second_num)
-            Initializes Calculator
-        add(self)
-            Adds two float numbers
-        sub(self)
-            Subtracts the second number from the first number
-        mul(self)
-            Multiplies two numbers
-        div(self)
-            Divides the first number by the second number
+Methods
+__init__(self, first_num, second_num)
+    Initializes Calculator
+add(self)
+    Adds two float numbers
+sub(self)
+    Subtracts the second number from the first number
+mul(self)
+    Multiplies two numbers
+div(self)
+    Divides the first number by the second number
     """
 
     first_num: float
     second_num: float
 
     def __init__(self, first_num, second_num):
-        """ This method initializes Calculator class with parameters a, b
+        """
+This method initializes Calculator class with parameters first_num, second_num
 
-    Parameters:
-    first_num  (float): the first number for performing operations
-    second_num (float): the second number for performing operations
-
-   """
+Parameters:
+first_num  (float): the first number for performing operations
+second_num (float): the second number for performing operations
+        """
         self.first_num = first_num
         self.second_num = second_num
 
     def add(self):
-        """ This method adds two float numbers
+        """
+This method adds two float numbers
 
-    Parameters:
-    first_num  (float): the first number for performing addition
-    second_num (float): the second number for performing addition
+Parameters:
+first_num  (float): the first number for performing addition
+second_num (float): the second number for performing addition
 
-    Returns:
-    float:Returning a sum of a and b
-
-   """
+Returns:
+float:Returning a sum of first_num and second_num
+        """
         return self.first_num + self.second_num
 
     def sub(self):
-        """ This method subtracts the second number from the first number
+        """
+This method subtracts the second number from the first number
 
-    Parameters:
-    first_num  (float): the first number for performing subtraction
-    second_num (float): the second number for performing subtraction
+Parameters:
+first_num  (float): the first number for performing subtraction
+second_num (float): the second number for performing subtraction
 
-    Returns:
-    float:Returning a difference of a and b
-
-   """
+Returns:
+float:Returning a difference of first_num and second_num
+        """
         return self.first_num - self.second_num
 
     def mul(self):
-        """ This method multiplies two numbers
+        """
+This method multiplies two numbers
 
-    Parameters:
-    first_num  (float): the first number for performing multiplication
-    second_num (float): the second number for performing multiplication
+Parameters:
+first_num  (float): the first number for performing multiplication
+second_num (float): the second number for performing multiplication
 
-    Returns:
-    float:Returning a product of a and b
-
-   """
+Returns:
+float:Returning a product of first_num and second_num
+        """
         return self.first_num * self.second_num
 
     def div(self):
-        """ This method divides the first number by the second number
+        """
+This method divides the first number by the second number
 
-    Parameters:
-    first_num  (float): the first number for performing division
-    second_num (float): the second number for performing division
+Parameters:
+first_num  (float): the first number for performing division
+second_num (float): the second number for performing division
 
-    Returns:
-    float:Returning a quotient of a and b
-
-   """
+Returns:
+float:Returning a quotient of first_num and second_num
+        """
         return self.first_num / self.second_num
 
 
-a = float(input('Please enter the first number : '))
-b = float(input('Please enter the second number : '))
-obj = Calculator(a, b)
+first_operand = float(input("Please enter the first number : "))
+second_operand = float(input("Please enter the second number : "))
+obj = Calculator(first_operand, second_operand)
 while True:
-    choice = int(input('Please select the options: \n1 - Addition \n2 - Subtraction '
-                       '\n3 - Multiplication \n4 - Division '))
+    choice = int(input("Please select the options: \n1 - Addition \n2 - Subtraction "
+                       "\n3 - Multiplication \n4 - Division "))
     if choice == 1:
         print("Result: ", obj.add())
     elif choice == 2:
@@ -113,8 +109,8 @@ while True:
     elif choice == 4:
         print("Result: ", obj.div())
     elif choice == 0:
-        print('Exit ')
+        print("Exit ")
         break
     else:
-        print('Incorrect entering, please, try again ')
+        print("Incorrect entering, please, try again ")
         break
